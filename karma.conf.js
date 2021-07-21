f17521468c4175346cfcb759ac40ace68c5d30a6
Shaun Sebastian <shaun445511@gmail.com>
2021-07-21 06:13:57 -0700

    GridView Bug Fixed

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Assignment-grid'),
+      dir: require('path').join(__dirname, './coverage/gridview'),
       subdir: '.',
       reporters: [
         { type: 'html' },
